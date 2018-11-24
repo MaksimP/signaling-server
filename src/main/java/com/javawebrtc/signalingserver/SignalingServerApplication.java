703b38b09db341d825b4adeba50c5740ecebc03e
MaksimP <maksimp86@mail.ru>
2018-11-24 22:05:23 +0200

    init commit

---
+++ src/main/java/com/javawebrtc/signalingserver/SignalingServerApplication.java
@@ -0,0 +1,12 @@
+package com.javawebrtc.signalingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignalingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignalingServerApplication.class, args);
+    }
+}
---
+++ src/main/java/com/javawebrtc/signalingserver/controller/ControllerClient.java
@@ -0,0 +1,13 @@
+package com.javawebrtc.signalingserver.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller("/")
+public class ControllerClient {
+
+    @GetMapping("/")
+    public String getClientPage() {
+        return "test";
+    }
+}
